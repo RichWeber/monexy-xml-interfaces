e2153102bf6bbb8d765ef4852328e46f9ee47423
RichWeber <rbagatyi@gmail.com>
2013-01-01 16:19:54 +0200

    Создаем MkTime и ApiHash

--- class.Monexy.php
+++ class.Monexy.php
@@ -10,4 +10,15 @@ class Monexy {
 	public $URL = 'https://www.monexy.com/xml/server.php?req=';
 	private $SecretKey = 'z80AhqYIuFP1';
 	
+	/*
+	 * Создаем MkTime и ApiHash
+	 */
+	private function MkTime()
+	{
+		$MkTime = $this->getmicrotime();
+		$this->ApiHash = sha1($this->ApiName . ':' 
+				. $this->SecretKey . ':' 
+				. $MkTime);
+		return $MkTime;
+	}
 }
