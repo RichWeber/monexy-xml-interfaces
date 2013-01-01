b98b6998d390b520521b9b63a5c256bf47c12f59
RichWeber <rbagatyi@gmail.com>
2013-01-01 16:26:34 +0200

    Подключаем класс для использования

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "Testing MoneXy API !!!";
+require_once 'class.Monexy.php';
+$api = new Monexy();
+
+echo 'Testing MoneXy API !!!';
+echo '<br /><br />=============================<br /><br />';
+
+echo $api->URL;
+echo '<br /><br />=============================<br /><br />';
 
 ?>
