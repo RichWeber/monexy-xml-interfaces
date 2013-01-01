8ee56d058e061446e5beea41054a47374ade4052
RichWeber <richweber@richweber-Lenovo-G575.(none)>
2013-01-01 16:08:19 +0200

    Создаем класс для работы с API Monexy

---
+++ class.Monexy.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Author RichWeber
+ * e-mail rbagatyi@gmail.com
+ */
+
+class Monexy {
+	
+	public $ApiName = '';
+	public $URL = 'https://www.monexy.com/xml/server.php?req=';
+	private $SecretKey = '';
+	
+}
